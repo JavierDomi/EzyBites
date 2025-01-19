12275c92dc258f6ce8b9ad78b6d404972d935e0a
NoxGlyphs <jdonosocam@gmail.com>
2025-01-19 21:44:33 +0100

    Community

--- app/src/main/java/com/dam/ezybites/ui/community/CommunityFragment.java
+++ app/src/main/java/com/dam/ezybites/ui/community/CommunityFragment.java
@@ -1,17 +1,28 @@
 package com.dam.ezybites.ui.community;
 
+import androidx.annotation.RequiresApi;
+import androidx.cardview.widget.CardView;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.graphics.RenderEffect;
+import android.graphics.Shader;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.UnderlineSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.dam.ezybites.R;
 
 public class CommunityFragment extends Fragment {
@@ -22,11 +33,35 @@ public class CommunityFragment extends Fragment {
         return new CommunityFragment();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.S)
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_community, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_community, container, false);
+
+        CardView cardBlur = view.findViewById(R.id.lol);
+
+        RenderEffect blurEffect = RenderEffect.createBlurEffect(10f, 10f, Shader.TileMode.CLAMP);
+        cardBlur.setRenderEffect(blurEffect);
+
+        TextView textView = view.findViewById(R.id.textView); // Asegúrate de usar el ID correcto de tu TextView
+
+// Texto completo
+        String text = "Recetas by Jorge Tesch";
+
+// Crear un SpannableString
+        SpannableString spannableString = new SpannableString(text);
+
+// Subrayar solo "Jorge Tesch"
+        int start = text.indexOf("Jorge Tesch");
+        int end = start + "Jorge Tesch".length();
+        spannableString.setSpan(new UnderlineSpan(), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+// Establecer el texto en el TextView
+        textView.setText(spannableString);
 
 
+        return view;
     }
 }
